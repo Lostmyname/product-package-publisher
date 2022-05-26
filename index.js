066e03e15d7ae5925e3207deb36abb54de8d450c
Aimy <aimy@wonderbly.com>
2022-05-26 12:43:21 +0100

    run the action in node which then executes a shell script

---
+++ index.js
@@ -0,0 +1,10 @@
+const shell = require('shelljs');
+
+shell.exec('./entrypoint.sh', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.error(`stderr: ${stderr}`);
+});
